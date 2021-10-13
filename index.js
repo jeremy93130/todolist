883228787248c0dedebe47c9f7ce71377c8cd012
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-13 14:33:37 +0200

    Tentatives infructueuses de STYLISER UN STATUS

--- index.js
+++ index.js
@@ -21,21 +21,41 @@ garbage()
 
 // Explications de Benoit sur l'envoie de tâches dans le tableau
 
-var list = document.getElementById("boxes")
+var boxes = document.getElementById("boxes")
 
-var task = []
+var tasks = []
 
-function onTaskSubmit(tasks) {
+function onTaskSubmit() {
     var inputValue = document.getElementById("newTask").value
-    tasks = { value: inputValue, status: "to do" }
+    var task = { value: inputValue, status: "to do" }
     tasks.push(task)
 
-    console.log(task)
-    debbuger
 
+    boxes.innerHTML = ""
 
-
-    task.forEach(function (task) {
-        list.innerHTML += list.innerHTML
+    tasks.forEach(function (task) {
+        boxes.innerHTML += `
+   
+<div class="justify">
+        <span>${task.status}</span>
+        ${task.value}
+     <div>
+        <div class="btn-group">
+            <button type="button" onclick="modified()" class="btn text-white modif">Modifier</button>
+            <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                <span class="visually-hidden">Toggle Dropdown</span>
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">To Do</a></li>
+                <li><a class="dropdown-item" href="#">Doing</a></li>
+                <li><a class="dropdown-item" href="#">Done</a></li>
+            </ul>
+                
+        </div>
+        <button class="delete" onclick="garbage()"><img src="/img/delete.png" alt="poubelle supprimer"></button>
+    </div>
+        
+</div>`
     })
 }
