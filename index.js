4fd57f838d56c14965356ffb25e7646be0599eac
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-13 22:09:31 +0200

    ajout de commentaires

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var buttons = document.getElementsByClassName("modif")
 var del = document.getElementsByClassName("delete")
+/**
+ * Element conteneur de toutes les tâches 
+ * @global
+ * @var {HTMLElement}
+ */
 var boxes = document.getElementById("boxes")
 var reinit = document.getElementById("writing")
 // var deleteTask = document.getElementsByClassName("delete")
 
 
-// Explications de Benoit sur l'envoie de tâches dans le tableau
+// Explications de Benoit sur l'envoi de tâches dans le tableau
 
 var tasks = []
 
@@ -22,34 +27,38 @@ function onTaskSubmit() {
 }
 /**
  * Definit le nouveau status de la tâche sélectionnée
- * @param {integer} index - L'index de la task 
- * @param {DOMElement} element - L'élément HTML sur lequel j'ai cliqué (to do, doing , done)
+ * @param {number} index - L'index de la task 
+ * @param {HTMLElement} element - L'élément HTML sur lequel j'ai cliqué (to do, doing , done)
  * @param {string} className - le nom de la class correspondante (voir)
  */
 function selectStatus(index, element, className) {
-    console.log("index= ", index)
-    console.dir(element.innerText)
     var task = tasks[index]
     task.status = element.innerText
-    console.dir(task)
     var span = document.getElementById(`status[${index}]`) // Va chercher dans le dom l'élément de l'id 
-    console.dir(span)
     span.innerHTML = task.status
     span.className = "status " + className //Changement de l'attribut class de l'élément span (changement couleur todo , doing , done) (voir ligne 43)
-    
+    selectStatus()
 
 }
 
-// Ligne 48 : Création d'une span de class 'status todo' 
+// Ligne 56 : Création d'une span de class 'status todo' 
+// Ligne 47 : Donne un nom à la div conteneuse
+
+/**
+ * Ajout d'une nouvelle tâche
+ * @param {Object} task - Tâche à ajouter
+ * @param {int} index - L'index de la tâche dans le tableau [tasks]
+ */
 function addTask(task, index) {
+    /*Ajout du code HTML de la tâche en fin de conteneur contrairement à .innerHTML qui remplace tout le code html*/
     boxes.insertAdjacentHTML("beforeend",
 
-        ` <div class="justify">
+        ` <div id="task[${index}]" class="justify">
         <span class="status todo" id="status[${index}]">${task.status}</span> 
         <span class="value">${task.value}</span>
     <div>
         <div class="btn-group">
-            <button type="button" onclick="modified(${index})" class="btn text-white modif">Modifier</button>
+            <button type="button" class="btn text-white modif">Modifier</button>
             <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split"
                 data-bs-toggle="dropdown" aria-expanded="false">
                 <span class="visually-hidden">Toggle Dropdown</span>
@@ -70,20 +79,8 @@ function addTask(task, index) {
 
 // Fonction pour supprimer une tâche
 function remove(index) {
-    tasks.splice(index, 1)
-    
-}
-
-// Fonction pour modifier une tâche
-
-function modified() {
-    var edit = document.getElementById("boxes")
-
-    var edited = edit.split().join("")
-    console.log(edited)
-
-    var task = { value: edit[boxes], status: "doing" }
-
+    tasks[index] = null // Remplace l'élément à l'index par null (rien du tout) , permet d'éviter de déplacer tous les autres index.
+    document.getElementById(`task[${index}]`).remove()
 
 }
 
@@ -92,11 +89,10 @@ function modified() {
 
 function randomButton() {
     var randomWords = ["Promener Kiwi", "Laver Mamie", "Renverser papy", "Reveiller son cerveau", "Recharger son téléphone", "Demander des grossièretées en italien à Salwa", "Organiser un combat de Sumo", "Visiter Fleury-Mérogis", "Faire chier Benoît toutes les 5 minutes", "Manger du pudding", "Faire un barbecu à l'eau"]
-    var maths = Math.floor(Math.random() * (randomWords.length));
+    var maths = Math.floor(Math.random() * (randomWords.length)); 
 
     var task = { value: randomWords[maths], status: "To Do" }
 
     addTask(task, tasks.length)
     tasks.push(task)
-    console.log(task)
 }
