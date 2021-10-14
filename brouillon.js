7912a4442a9b40732f3f28291710ac24da3127a2
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-14 15:20:12 +0200

    Brouillon

--- brouillon.js
+++ brouillon.js
@@ -1,8 +1,6 @@
-var buttons = document.getElementsByClassName("modif")
 var del = document.getElementsByClassName("delete")
 var boxes = document.getElementById("boxes")
 var reinit = document.getElementById("writing")
-// var deleteTask = document.getElementsByClassName("delete")
 
 
 // Explications de Benoit sur l'envoie de tâches dans le tableau
@@ -22,26 +20,26 @@ function onTaskSubmit() {
 }
 
 
-function displayBoxes() {
+function displayBoxes(index, task) {
     boxes.innerHTML = ""
 
     tasks.forEach(function (task, index) {
         boxes.innerHTML += `
    
-<div class="justify">
-        <span class="status">${task.status}</span>
-        ${task.value}
-    <div>
+<div id="task[${index}]" class="justify">
+        <span class="status todo" id="status[${index}]">${task.status}</span>
+        <span id="edit-${index}"class="value">${task.value}</span>
+    <div class="sideToSide">
         <div class="btn-group">
-            <button type="button" onclick="modified(${index})" class="btn text-white modif">Modifier</button>
+            <button type="button"  class="btn text-white" onclick="addForm(${index})">Modifier</button>
             <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split"
                 data-bs-toggle="dropdown" aria-expanded="false">
                 <span class="visually-hidden">Toggle Dropdown</span>
             </button>
             <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">To Do</a></li>
-                <li><a class="dropdown-item" href="#">Doing</a></li>
-                <li><a class="dropdown-item" href="#">Done</a></li>
+                <li><a class="dropdown-item" href="#" onclick="selectStatus(${index}, this, 'todo')">To Do</a></li>
+                <li><a class="dropdown-item" href="#" onclick="selectStatus(${index}, this, 'doing')">Doing</a></li>
+                <li><a class="dropdown-item" href="#" onclick="selectStatus(${index}, this, 'done')">Done</a></li>
             </ul>
                 
         </div>
@@ -53,25 +51,73 @@ function displayBoxes() {
     })
 }
 
+// Fonction qui permet de changer le status de To Do , Doing ou Done:
+
+function selectStatus(index, element, className) {
+
+    // Tâche dont on modifie le statut :
+
+    var task = tasks[index]
+
+    task.status = element.innerText
+
+    var span = document.getElementById(`status[${index}]`)
+    span.innerHTML = task.status
+
+    span.className = 'status' + className
+    selectStatus()
+
+}
+
 // Fonction pour supprimer une tâche
 function remove(index) {
     tasks.splice(index, 1)
     displayBoxes()
 }
 
-// Fonction pour modifier une tâche
+//Cette fonction permet d'afficher le formulaire de modification
 
+function addForm(index) {
+    // on récupere l'element où on va rajouter notre formulaire
+    var edited = document.getElementById(`edit-${index}`)
+    console.log(edited)
 
+    // Ajout du formulaire à l'element 
+    edited.innerHTML = `
+        <form onsubmit="edit(${index}); return false;" >
+            <input type="text" id="taskEdited" class="sendList" placeholder="">
+            <button id="edit-btn" type="submit">Valider</button>
+        </form>
+    `
+
+}
+
+// Cette fonction est appellée dans le formulaire pour modifier l'Objet 
+
+function edit(index) {
+    // on récupere la valeur de l'input 
+    var edited = document.getElementById("taskEdited").value
+
+    // On change la valeur de l'Objet qui est dans le tableau task avec la nouvelle valeur (ligne au dessus)
+    tasks[index].value = edited
+
+    // On récupere l'élément où l'on veut réafficher
+    var elementHTML = document.getElementById(`edit-${index}`)
+
+    // Je supprime tout ce qui était dedans et initialise une nouvelle string vide 
+    elementHTML.innerHTML = "";
+
+    // A la place de string vide je mets la nouvelle valeur ( la value de l'Objet modifié )
+    elementHTML.innerHTML = tasks[index].value;
+}
 
 // Fonction bouton random : 
 
 function randomButton(random) {
     var randomWords = ["Promener Kiwi", "Laver Mamie", "Renverser papy", "Reveiller son cerveau", "Recharger son téléphone", "Demander des grossièretées en italiens à Salwa", "Organiser un combat de Sumo", "Visiter Fleury-Mérogis", "Faire chier Benoît toutes les 5 minutes"]
-    var maths = Math.floor(Math.random() * ());
+    var maths = Math.floor(Math.random() * (randomWords.length));
 
-    console.log(randomWords[maths])
-    
-    var task = {value: randomWords[maths], status: "To Do"}
+    var task = { value: randomWords[maths], status: "To Do" }
 
     tasks.push(task)
 
@@ -100,16 +146,16 @@ function addForm(index) {
 function edit(index) {
     // on récupere la valeur de l'input 
     var edited = document.getElementById("taskEdited").value
-    
+
     // On change la valeur de l'Objet qui est dans le tableau task avec la nouvelle valeur (ligne au dessus)
     tasks[index].value = edited
-    
+
     // On récupere l'élément où l'on veut réafficher
     var elementHTML = document.getElementById(`edit-${index}`)
 
     // Je supprime tout ce qui était dedans et initialise une nouvelle string vide 
-    elementHTML.innerHTML =  "";
+    elementHTML.innerHTML = "";
 
     // A la place de string vide je mets la nouvelle valeur ( la value de l'Objet modifié )
-    elementHTML.innerHTML =  tasks[index].value;
+    elementHTML.innerHTML = tasks[index].value;
 }
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var button = document.getElementsByClassName("modif")
 var del = document.getElementsByClassName("delete")
 /**
  * Element conteneur de toutes les tâches 
@@ -75,7 +74,7 @@ function addTask(task, index) {
         <span id="edit-${index}" class="value">${task.value}</span>
     <div class="sideToSide">
         <div class="btn-group">
-            <button type="button" class="btn text-white modif" onclick="addForm(${index})">Modifier</button>
+            <button type="button" class="btn text-white" onclick="addForm(${index})">Modifier</button>
             <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split"
                 data-bs-toggle="dropdown" aria-expanded="false">
                 <span class="visually-hidden">Toggle Dropdown</span>
@@ -116,7 +115,7 @@ function randomButton() {
     tasks.push(task)
 }
 
-// Cette fonction permet d'afficher le formulaire 
+// Cette fonction permet d'afficher le formulaire de modification
 
 function addForm(index) {
     // on récupere l'element où on va rajouter notre formulaire
